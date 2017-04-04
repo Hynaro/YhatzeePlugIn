005bcc9c4dfb4a81f7ed542f9484007ff1f696c3
Bertrand Tissier <berb.tissier@wanadoo.fr>
2017-04-04 09:51:47 +0200

    ajout de la classe Evaluateur, jededes, joueur, ligne

---
+++ src/Joueur.java
@@ -0,0 +1,55 @@
+
+public class Joueur {
+	
+	String nom;
+	int highscore;
+	int partieGagnees;
+	int partiePerdues;
+	
+	public Joueur(String nom) {
+		super();
+		this.nom = nom;
+		this.highscore = 0;
+		this.partieGagnees = 0;
+		this.partiePerdues = 0;
+	}
+
+	public Joueur() {
+	
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getHighscore() {
+		return highscore;
+	}
+
+	public void setHighscore(int highscore) {
+		this.highscore = highscore;
+	}
+
+	public int getPartieGagnees() {
+		return partieGagnees;
+	}
+
+	public void setPartieGagnees(int partieGagnees) {
+		this.partieGagnees = partieGagnees;
+	}
+
+	public int getPartiePerdues() {
+		return partiePerdues;
+	}
+
+	public void setPartiePerdues(int partiePerdues) {
+		this.partiePerdues = partiePerdues;
+	}
+	
+	
+
+}
---
+++ src/Ligne.java
@@ -0,0 +1,36 @@
+
+public class Ligne {
+
+	String nom;
+	int score;
+	
+	public Ligne() {
+
+	}
+
+	public Ligne(String nom, int score) {
+		this.nom = nom;
+		this.score = score;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	public int calcul(){
+		return 0;
+	}
+	
+}
